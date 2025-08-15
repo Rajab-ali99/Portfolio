74f201012e054ccbf835a758bcbae87304603d65
Rajab Ali <168520584+Rajab-ali99@users.noreply.github.com>
2025-08-15 17:02:35 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
       <nav className='flex justify-between lg:px-10 pr-4  lg:justify-around h-14 items-center overflow-hidden bg-violet-700  '>
 
-        <div className="logo h-[75px] bg-violet-700 "><img className=' bg-violet-700 relative top-1  h-full w-full object-cover  ' src='../src/assets/logo1.png' alt="error"  /></div>
+        <div className="logo h-[75px] bg-violet-700 "><img className=' bg-violet-700 relative top-1  h-full w-full object-cover  ' src='./assets/logo1.png' alt="error"  /></div>
         
         <div onClick={handleclick} className='block lg:hidden cursor-pointer bg-violet-700 '>
           <GiHamburgerMenu className='bg-violet-700 ' />
